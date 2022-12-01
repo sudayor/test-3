f700dc3e66367a2457f43b65ade64844355c432b
user <atabekabdusharipov@gmail.com>
2022-12-01 17:48:48 +0500

    first commit

---
+++ JS/main.js
@@ -0,0 +1,91 @@
+//savolar
+// 1
+//var 
+// let
+// const
+
+// 2
+// "string", number 
+
+//3
+// increment
+// ++a;        
+// a++;     
+// decrement   
+// --a;        
+// a--;        
+
+//4
+// "string"+Number= "stringNumber"
+
+// 5
+// && va sozi manosini beradi ikkitasidan birini tanlaydi
+// || yoki sozini beradi ushbu yoki uni yoki buni
+
+// 6
+// let a=1
+// let b=2
+// if (a>b) {
+//   return a 
+// }
+// else{
+//   return b
+// }
+// console.log();
+
+// 7
+// else if(a===b){
+// console.log("a bilan b teng");
+// },
+
+// 8
+// var arr=["A","B","C"]
+// console.log(arr[2]);
+
+// 9
+// let userName ="Xudayor"
+// let userLastName ="Bekturdiev"
+// let userAgr = 15
+
+// let user = {
+//   name:"Xudayor",
+//   lastName:"Bekturdiev",
+//   Agr:15,
+//    adress:{
+//    street:"Al-Xorazmiy"
+//    house: 75
+//    },
+//    location[12.258.236.123.456.789]
+//  }
+// let a="hello"
+// let b= a
+// console.log(a,b );
+
+// 10
+// typeof va boshqa shunga o`hshash larda metod qolanadi
+
+// Masalalar
+
+// 1
+// function salom(a,b){
+// console.log(a+b);
+// }
+// console.log(22,5);
+
+
+// 2
+// function age(yosh) {
+//   return 2008-yosh
+// }
+// console.log(age);
+
+// 3
+// function  myFunc(a, b){
+//   if (typeof(a) === "number" && typeof(b) === 'number') {
+//     console.log(a+b)
+//   }
+//   else{
+//    console.log("son emas")
+//   }
+// }
+// myFunc(90,"jhfgjsdf")
